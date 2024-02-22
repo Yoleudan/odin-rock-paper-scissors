8e2bf8c70a49359e47d98be59f22d30b251cdf45
yoleudan <158090797+Yoleudan@users.noreply.github.com>
2024-02-22 15:08:48 +0000

    Make function to convert randint into choice

--- main.js
+++ main.js
@@ -2,5 +2,20 @@ function getRandomInt(max){
     return Math.floor(Math.random()*max)
 }
 
-let computerChoice = getRandomInt(3)
+function convertInt(){
+    if (computerInt === 0){
+        return "rock";
+    }
+
+    else if(computerInt === 1){
+        return "paper"
+    }
+    else if(computerInt === 2){
+        return "scissors"
+    }
+}
+
+let computerInt = getRandomInt(3)
+
+let computerChoice = convertInt()
 console.log(computerChoice)
